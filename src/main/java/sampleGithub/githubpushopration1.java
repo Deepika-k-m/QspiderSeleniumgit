ab302d7bfb22777408580b6214a94ec5d2404b3c
Deepika-k-m <kmdeepika501@gmail.com>
2025-03-29 15:31:19 +0530

    first done changes in the code present in local system

---
+++ src/main/java/sampleGithub/githubpushopration1.java
@@ -0,0 +1,10 @@
+package sampleGithub;
+
+import org.testng.annotations.Test;
+
+public class githubpushopration1 {
+@Test
+public void firstOpration() {
+	System.out.println("first do updates in the code in the local system");
+}
+}
